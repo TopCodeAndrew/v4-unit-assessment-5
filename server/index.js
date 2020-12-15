af1affca8726e78d56858de7934435c1935759bb
Andrew Smith <topcodeandrew@gmail.com>
2020-12-15 10:07:02 -0700

    starting Authentication section

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+const massive = require('massive');
 const express = require('express'),
-      userCtrl = require('./controllers/user'),
-      postCtrl = require('./controllers/posts')
+    userCtrl = require('./controllers/user'),
+    postCtrl = require('./controllers/posts')
 
 
 const app = express();
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env
 
 app.use(express.json());
 
@@ -14,10 +17,23 @@ app.post('/api/auth/login', userCtrl.login);
 app.get('/api/auth/me', userCtrl.getUser);
 app.post('/api/auth/logout', userCtrl.logout);
 
+
 //Post Endpoints
 app.get('/api/posts', postCtrl.readPosts);
 app.post('/api/post', postCtrl.createPost);
 app.get('/api/post/:id', postCtrl.readPost);
 app.delete('/api/post/:id', postCtrl.deletePost)
 
-app.listen(4000, _ => console.log(`running on ${4000}`));
+
+
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+}).then((dbInstance) => {
+    console.log('DB Ready')
+    app.set('db', dbInstance)
+    app.listen(SERVER_PORT, () => console.log(`running on PORT ${SERVER_PORT}`));
+})
